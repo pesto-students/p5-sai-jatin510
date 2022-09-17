3553ab7cf9db3f6aa4c7b36567f2b42dc8456ab8
jatin510 <jatin6972@gmail.com>
2022-09-17 12:00:42 +0530

    added test case in mathOperation.test.ts

--- Week-5/assignment-5.4/mathOperation.test.js
+++ Week-5/assignment-5.4/mathOperation.test.js
@@ -1,12 +1,24 @@
 const mathOperations = require('./mathOperations');
 
 describe('To test the math functions', () => {
+  test('mathOperations is an object containing three functions: sum, diff and product', () => {
+    expect(mathOperations).toEqual(
+      expect.objectContaining({
+        sum: expect.any(Function),
+        diff: expect.any(Function),
+        product: expect.any(Function),
+      })
+    );
+  });
+
   test('sum operation', () => {
     expect(mathOperations.sum(10, 5)).toEqual(15);
   });
+
   test('difference operation', () => {
     expect(mathOperations.diff(15, 5)).toEqual(10);
   });
+
   test('product operation', () => {
     expect(mathOperations.product(10, 5)).toEqual(50);
   });
