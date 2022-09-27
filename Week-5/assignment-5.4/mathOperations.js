8bea2b2ddf12457bf16659248d157ab6a2f741ba
jatin510 <jatin6972@gmail.com>
2022-09-27 20:57:07 +0530

    added checks in mathOperation.js and added test to check if the input are numbers or not

--- Week-5/assignment-5.4/mathOperations.js
+++ Week-5/assignment-5.4/mathOperations.js
@@ -1,11 +1,25 @@
 const mathOperations = {
+  isValidateInput: function (a, b) {
+    return (
+      typeof a === 'number' && typeof b === 'number' && !(isNaN(a) || isNaN(b))
+    );
+  },
+
   sum: function (a, b) {
+    if (!this.isValidateInput(a, b)) throw new TypeError('Invalid input');
+
     return a + b;
   },
+
   diff: function (a, b) {
+    if (!this.isValidateInput(a, b)) throw new TypeError('Invalid input');
+
     return a - b;
   },
+
   product: function (a, b) {
+    if (!this.isValidateInput(a, b)) throw new TypeError('Invalid input');
+
     return a * b;
   },
 };
