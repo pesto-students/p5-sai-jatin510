9e1c1c40f6afdfdd8b5f5d3c5a3f38b8f8fe1ec7
jatin510 <jatin6972@gmail.com>
2022-09-27 17:23:02 +0530

    problem 6: find triplet sum which is closest to the target

--- Week-6/problem6.js
+++ Week-6/problem6.js
@@ -1,1 +1,25 @@
-// find triplet in array with the given sum
+// find triplet sum in array closest to the target
+
+const arr = [-1, 2, 1, -4];
+const target = 1;
+
+let closest = Number.MAX_SAFE_INTEGER;
+const n = arr.length;
+
+for (let i = 0; i < n; i++) {
+  const firstElem = arr[i];
+
+  for (let j = i + 1; j < n; j++) {
+    const secondElem = arr[j];
+
+    for (let k = j + 1; k < n; k++) {
+      const thirdElem = arr[k];
+      const sum = firstElem + secondElem + thirdElem;
+      if (Math.abs(sum - target) < Math.abs(closest - target)) {
+        closest = sum;
+      }
+    }
+  }
+}
+
+console.log(closest);
