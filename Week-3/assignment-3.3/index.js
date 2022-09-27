7498312d0e500282cea6211bd5301f7b752cd281
jatin510 <jatin6972@gmail.com>
2022-09-27 20:41:40 +0530

    added explanation for the problem which is based on the concept of closure.

--- Week-3/assignment-3.3/index.js
+++ Week-3/assignment-3.3/index.js
@@ -15,7 +15,13 @@ increment();
 increment();
 increment();
 log();
+// What is logged?
 
+// The output of the program will be "Count is 0".
+// When we call the the log function, the string will get the value of the count variable are 0.
+// THe message string is will get the memory with value of count as 0.
+
+//
 // this will return correct output
 {
   function createIncrement() {
