4df90172640aad8936f9836f13fe18fcf27718b0
jatin510 <jatin6972@gmail.com>
2022-10-08 17:21:47 +0530

    spiral print 2-d matrix

---
+++ Week-6/problem2.js
@@ -0,0 +1,50 @@
+// spiral print matrix
+
+const matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+
+function spiralOrder(matrix) {
+  let result = [];
+  let left = 0;
+  let right = matrix[0].length - 1;
+  let top = 0;
+  let bottom = matrix.length - 1;
+
+  let direction = 'right';
+
+  while (left <= right && top <= bottom) {
+    if (direction === 'right') {
+      for (let i = left; i <= right; i++) {
+        result.push(matrix[top][i]);
+      }
+      top += 1;
+      direction = 'down';
+    } else if (direction === 'down') {
+      for (let i = top; i <= bottom; i++) {
+        result.push(matrix[i][right]);
+      }
+      right -= 1;
+      direction = 'left';
+    } else if (direction === 'left') {
+      for (let i = right; i >= left; i--) {
+        result.push(matrix[bottom][i]);
+      }
+      bottom -= 1;
+      direction = 'up';
+    } else if (direction === 'up') {
+      for (let i = bottom; i >= top; i--) {
+        result.push(matrix[i][left]);
+      }
+      left += 1;
+      direction = 'right';
+    }
+  }
+  return result;
+}
+
+const result = spiralOrder(matrix);
+console.log(result);
