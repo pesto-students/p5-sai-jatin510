82c8d73384e0c85645020f32cdf76949e34fa9e0
jatin510 <jatin6972@gmail.com>
2022-11-01 10:57:17 +0530

    added todo component

--- Week-9/todolist/src/components/Todos.tsx
+++ Week-9/todolist/src/components/Todos.tsx
@@ -5,10 +5,19 @@ import todoListData from '../data/todolist.json';
 function Todos() {
   const [todoList, setTodoList] = useState(todoListData);
 
+  const handleToggle = (id: any) => {
+    let mapped = todoList.map((task) => {
+      return task.id == id
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
+    setTodoList(mapped);
+  };
+
   return (
     <>
       {todoList.map((todo) => {
-        return <Todo key={todo.id} todo={todo} />;
+        return <Todo key={todo.id} todo={todo} handleToggle={handleToggle} />;
       })}
     </>
   );
