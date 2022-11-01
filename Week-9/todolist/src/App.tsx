12b22d39815cc756ab9807b0002f1faa3fdcf8e0
jatin510 <jatin6972@gmail.com>
2022-11-01 11:01:08 +0530

    moved state back to app.tsx

--- Week-9/todolist/src/App.tsx
+++ Week-9/todolist/src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import TodoForm from './components/TodoForm';
 import Todos from './components/Todos';
+import todoListData from './data/todolist.json';
 
 function App() {
+  const [todoList, setTodoList] = useState(todoListData);
+
+  const handleToggle = (id: any) => {
+    let mapped = todoList.map((task) => {
+      return task.id == id
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
+    setTodoList(mapped);
+  };
+
+  const addTodo = (userInput: any) => {
+    setTodoList([
+      ...todoList,
+      { id: todoList.length + 1, task: userInput, complete: false },
+    ]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Todos />
+      <Todos todoList={todoList} handleToggle={handleToggle} />
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
--- Week-9/todolist/src/components/Todos.tsx
+++ Week-9/todolist/src/components/Todos.tsx
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
 import Todo from './Todo';
-import todoListData from '../data/todolist.json';
 
-function Todos() {
-  const [todoList, setTodoList] = useState(todoListData);
-
-  const handleToggle = (id: any) => {
-    let mapped = todoList.map((task) => {
-      return task.id == id
-        ? { ...task, complete: !task.complete }
-        : { ...task };
-    });
-    setTodoList(mapped);
-  };
+const Todos = (props: any) => {
+  const { todoList, handleToggle } = props;
 
   return (
     <>
-      {todoList.map((todo) => {
+      {todoList.map((todo: any) => {
         return <Todo key={todo.id} todo={todo} handleToggle={handleToggle} />;
       })}
     </>
   );
-}
+};
 
 export default Todos;
